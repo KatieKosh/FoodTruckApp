6294365e04205da60a4a42f27e9f89d8a6cb67c0
KatieKosh <kkoshinsnki@hotmail.com>
2017-03-07 18:57:31 -0600

    most recent changes

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+<script src="https://www.gstatic.com/firebasejs/3.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCJTl8cq8DowvcmmXiAP5ukLsDISAu0r8A",
+    authDomain: "foodtruckapp-58cd4.firebaseapp.com",
+    databaseURL: "https://foodtruckapp-58cd4.firebaseio.com",
+    storageBucket: "foodtruckapp-58cd4.appspot.com",
+    messagingSenderId: "114520212317"
+  };
+  firebase.initializeApp(config);
+</script>
